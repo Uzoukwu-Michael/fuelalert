c15de5247a483760a2eae66ce8a2e75427e20277
sammyifeanyi <145140412+sammyifeanyi@users.noreply.github.com>
2023-10-10 12:29:49 +0100

    This is my first fuel alert project
    Let Work together

---
+++ main.js
@@ -0,0 +1,13 @@
+const reg = document.getElementById('submitt');
+const input = document.getElementById('input');
+
+function register(){
+    let b = input.value;
+    console.log(b);
+}
+
+
+
+
+
+
